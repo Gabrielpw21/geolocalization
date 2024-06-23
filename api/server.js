cb3209665994fdbba7521245efe976fd5688b41b
gabriel alves <gabrielpw212@gmail.com>
2024-06-23 19:34:37 -0400

    geolocalização

--- api/server.js
+++ api/server.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Serve arquivos estáticos da pasta 'public'
-app.use(express.static('../public'));
+// Servir arquivos estáticos da pasta 'public'
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // Armazenar localizações dos usuários
 let usersLocations = {};
 
 io.on('connection', (socket) => {
-    console.log('Novo cliente conectado:', socket.id, Date.UTC());
+    console.log('Novo cliente conectado:', socket.id);
 
     socket.on('updateLocation', (location) => {
         usersLocations[socket.id] = location;
         io.emit('locationUpdate', usersLocations);
-        console.log(usersLocations)
     });
 
     socket.on('disconnect', () => {
@@ -28,6 +28,7 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('Servidor iniciado na porta 3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Servidor iniciado na porta ${PORT}`);
 });
